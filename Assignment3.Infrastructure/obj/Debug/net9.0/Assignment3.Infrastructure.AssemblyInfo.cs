8b16409e90050369b7d5aa4751c055110a2d7289
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2025-11-19 10:21:28 -0500

    deployment to Azure

--- Assignment3.Infrastructure/obj/Debug/net9.0/Assignment3.Infrastructure.AssemblyInfo.cs
+++ Assignment3.Infrastructure/obj/Debug/net9.0/Assignment3.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae77c8e877e0251f08696e09d67cfac7d62d77a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51968061531fb9dc1d09e85000c98497fb35e73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
